ba5e54098bb7de09d04800b64d4818312a376c82
Leonardo <lcsilva80@yahoo.com.br>
2017-08-30 13:21:08 -0300

    Add: Point System

--- js/just.js
+++ js/just.js
@@ -4,7 +4,13 @@ var justapp = angular.module('marvel', []);
 
 justapp.controller("quetionaryController", function($scope, $http) {
     $scope.marvel = [];
-    $scope.userGiveUp = [];
+    $scope.userGiveUp = false;
+    $scope.heroPic = '';
+    $scope.heroName = '';
+    $scope.ButtonNext = '';
+    $scope.userPoints = 0;
+    $scope.choice = true;
+    var x = 0;
     var publicKey = 'c65af5cea0a8039ef261b24a695d602c';
     var baseUrl = "http://gateway.marvel.com/v1/public/characters";
     var timeStamp = '17071991';
@@ -17,51 +23,70 @@ justapp.controller("quetionaryController", function($scope, $http) {
                 apikey: publicKey,
                 hash: hash,
                 limit: 50,
-                series: '2, 3, 5, 9, 22, 32'
+                series: '5, 22, 102, 99, 91, 47, 44, 160, 173'
+                    /*On Series, I bring some heres based in the sequencial series*/
             }
         }).then(function(response) {
             console.log(response.data.data.results);
             $scope.marvel = response.data.data.results;
+            $scope.loadHero();
         });
     }
 
-    function clearName(idInput) {
-        document.getElementById(idInput).value = '';
+    $scope.loadHero = function(HeroChoice) {
+        if (HeroChoice) {
+            x += HeroChoice;
+        }
+
+        $scope.heroPic = $scope.marvel[x].thumbnail.path + '.' + $scope.marvel[x].thumbnail.extension;
+        $scope.heroName = $scope.marvel[x].name;
+
+        $scope.userGiveUp = false;
+        $scope.choice = true;
     }
 
-    $scope.giveUp = function(id, i) {
-        if ($scope.marvel[i].id == id)
-            $scope.userGiveUp[i] = id;
+    function clearName() {
+        document.getElementById('userChoice').value = '';
     }
 
-    $scope.submit = function(id, i) {
-        var HerosName = document.getElementById(id).value;
-        document.getElementById('1009367').value.substring(0, document.getElementById('1009367').value.indexOf('(') - 1)
+    $scope.giveUp = function(wa) {
+        if (wa) {
+            $scope.ButtonNext = "Oh Nooo, Wrong answer, Let's try the next hero"
+        } else {
+            $scope.ButtonNext = "Ok, Don't worry, you can try the next hero"
+        }
 
-        for (var i = 0; $scope.marvel.length > i; i++) {
-            if ($scope.marvel[i].id == id) {
-                if ($scope.marvel[i].name.indexOf('(') != '-1') {
-                    var nomeMarvel = $scope.marvel[i].name.substring(0, $scope.marvel[i].name.indexOf('(') - 1);
-                } else {
-                    var nomeMarvel = $scope.marvel[i].name;
-                }
+        $scope.userGiveUp = true;
+        $scope.choice = false;
+        $scope.userPoints--;
+    }
 
-                if (nomeMarvel.toLowerCase() == HerosName.toLowerCase()) {
-                    //When correct show the name of the hero  
-                    $scope.userGiveUp[i] = true
-                        //Show to user, that's the answer is correct
-                    alert('Brilliant, you are correct!!!');
-                    //Clear the input
-                    clearName(id);
-                    break;
-                } else {
-                    alert('Oh Nooo! Wrong name, try again!');
-                    clearName(id);
-                    break;
-                }
-            }
+    $scope.submit = function() {
+        var HerosName = document.getElementById('userChoice').value;
+
+        if ($scope.marvel[x].name.indexOf('(') != '-1') {
+            var nomeMarvel = $scope.marvel[x].name.substring(0, $scope.marvel[x].name.indexOf('(') - 1);
+        } else {
+            var nomeMarvel = $scope.marvel[x].name;
+        }
+
+        if (nomeMarvel.toLowerCase() == HerosName.toLowerCase()) {
+            //Show to user, that's the answer is correct
+            $scope.ButtonNext = 'Nice job, Lets to the next hero';
+            //When correct show the name of the hero  
+            $scope.userGiveUp = true;
+            //Hide the buttons to avoid try insert a new answer on the same hero
+            $scope.choice = false;
+            //Add the point of correct answer
+            $scope.userPoints++;
+            //Clear the input
+            clearName();
+        } else {
+            $scope.giveUp('wrong');
+            clearName();
         }
     }
 
     getHero();
+
 });
